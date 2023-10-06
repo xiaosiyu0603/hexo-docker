a1bd83e1df62e72d921ab4cdb6f465e7d0b07f46
Xiao Siyu <xiaosiyu0603@qq.com>
2023-10-07 06:20:55 +0800

    Add Dockerfiles and shell scripts.

---
+++ node18.18.0-slim/build.sh
@@ -0,0 +1,1 @@
+docker build -t xiaosiyu0603/hexo:env-cli4.3.1-node18.18.0-slim .
---
+++ node18.18.0/run.sh
@@ -0,0 +1,1 @@
+docker run -it --rm -p 64000:4000 -v $(pwd):$(pwd) -w $(pwd) xiaosiyu0603/hexo:env-cli4.3.1-node18.18.0 # bash
